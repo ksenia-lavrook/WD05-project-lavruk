e5c0f9497253aa7ad3c0c38a4c1fafb609c635eb
Ksenia <ksenia.lavruk@yandex.ru>
2019-01-22 16:34:16 +0300

    saving project settings

---
+++ www/config.php
@@ -0,0 +1,16 @@
+<?php 
+
+// ВИ SETTINGS
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'WD05-project-lavruk');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Хост сайта
+define('HOST', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/');
+
+// Физический путь к корневой директории скрипта
+define('ROOT', dirname(__FILE__).'/');
+
+
+?>
---
+++ www/templates/_parts/_footer.tpl
@@ -0,0 +1,3 @@
+<header>
+	<h1>This ispage Footer - _footer.tpl</h1>
+</header>
